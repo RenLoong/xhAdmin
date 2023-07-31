2167cf789da1d6355b131e99e7c85ab9d492b4f6
楚羽幽 <cy958416459@qq.com>
2023-07-31 15:40:40 +0800

    1

---
+++ update/0.yc_system_admin_role_is_system.sql
@@ -0,0 +1,18 @@
+DROP PROCEDURE IF EXISTS system_admin_role_is_system;
+CREATE PROCEDURE system_admin_role_is_system()
+BEGIN
+	IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'yc_system_admin_role' AND column_name = 'is_system')
+	THEN
+		ALTER TABLE `yc_system_admin_role` ADD COLUMN `is_system` enum('10','20') NULL COMMENT '是否系统：10否 20是' AFTER `rule`;
+		UPDATE `yc_system_admin_role` SET `is_system` = '20' WHERE `id` = '1'
+		UPDATE `yc_system_admin_role` SET `is_system` = '10' WHERE `id` = ''
+	END IF;
+	IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'yc_system_admin_role' AND column_name = 'is_system')
+	THEN
+		ALTER TABLE `yc_system_admin_role` MODIFY COLUMN `is_system` enum('10','20') NULL COMMENT '是否系统：10否 20是' AFTER `rule`;
+		UPDATE `yc_system_admin_role` SET `is_system` = '20' WHERE `id` = '1'
+		UPDATE `yc_system_admin_role` SET `is_system` = '10' WHERE `id` = ''
+	END IF;
+END;
+
+CALL system_admin_role_is_system();
