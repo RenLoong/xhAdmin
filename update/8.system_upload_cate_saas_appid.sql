6c243405852ff04a6f083301bdfd10376a809729
楚羽幽 <cy958416459@qq.com>
2023-07-31 16:21:32 +0800

    --fix sql

--- update/8.system_upload_cate_saas_appid.sql
+++ update/8.system_upload_cate_saas_appid.sql
@@ -2,13 +2,13 @@ DROP PROCEDURE IF EXISTS system_upload_cate_saas_appid;
 
 CREATE PROCEDURE system_upload_cate_saas_appid()
 BEGIN
-	IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'yc_system_upload_cate' AND column_name = 'store_id')
+	IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'yc_system_upload_cate' AND column_name = 'saas_appid')
 	THEN
-		ALTER TABLE `yc_system_upload_cate` ADD COLUMN `saas_appid` int(11) NULL COMMENT '应用项目ID' AFTER `store_id`;
+		ALTER TABLE `yc_system_upload_cate` ADD COLUMN `saas_appid` int(10) NULL COMMENT '应用项目ID' AFTER `store_id`;
 	END IF;
-	IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'yc_system_upload_cate' AND column_name = 'store_id')
+	IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'yc_system_upload_cate' AND column_name = 'saas_appid')
 	THEN
-		ALTER TABLE `yc_system_upload_cate` MODIFY COLUMN `saas_appid` int(11) NULL COMMENT '应用项目ID' AFTER `store_id`;
+		ALTER TABLE `yc_system_upload_cate` MODIFY COLUMN `saas_appid` int(10) NULL COMMENT '应用项目ID' AFTER `store_id`;
 	END IF;
 END;
 
