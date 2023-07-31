12c59dc1045bf6d17058cc78fbe5a8adf7909963
楚羽幽 <cy958416459@qq.com>
2023-07-31 16:31:30 +0800

    1

--- update/10.users_saas_appid.sql
+++ update/10.users_saas_appid.sql
@@ -2,13 +2,9 @@ DROP PROCEDURE IF EXISTS users_saas_appid;
 
 CREATE PROCEDURE users_saas_appid()
 BEGIN
-	IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'yc_users' AND column_name = 'saas_appid')
+	IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'yc_store_app' AND column_name = 'appid')
 	THEN
-		ALTER TABLE `yc_users` ADD COLUMN `saas_appid` int(11) NULL COMMENT '应用项目ID' AFTER `store_id`;
-	END IF;
-	IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'yc_store_app' AND column_name = 'saas_appid')
-	THEN
-		ALTER TABLE `yc_users` MODIFY COLUMN `saas_appid` int(11) NULL COMMENT '应用项目ID' AFTER `store_id`;
+		ALTER TABLE `yc_users` CHANGE COLUMN `appid` `saas_appid` int(10) NULL DEFAULT NULL COMMENT '应用项目ID' AFTER `store_id`;
 	END IF;
 END;
 
