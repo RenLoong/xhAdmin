5c11df02b749d5ccbc6e6673de5bb9b8088dfbd6
楚羽幽 <cy958416459@qq.com>
2023-08-02 17:06:54 +0800

    1

--- public/install/data/sql/yc_store_app.sql
+++ public/install/data/sql/yc_store_app.sql
@@ -5,11 +5,11 @@ CREATE TABLE `yc_store_app`  (
   `create_at` datetime NULL DEFAULT NULL,
   `update_at` datetime NULL DEFAULT NULL,
   `store_id` int NULL DEFAULT NULL COMMENT '所属租户',
-  `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '应用名称',
-  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '应用网址，不带结尾斜杠',
+  `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '项目名称',
   `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '应用标识',
-  `logo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '应用LOGO',
-  `status` enum('10','20') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '10' COMMENT '应用状态',
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '项目网址，不带结尾斜杠',
+  `logo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '项目LOGO',
+  `status` enum('10','20') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '10' COMMENT '项目状态',
   `platform` enum('wechat','mini_wechat','douyin','h5','app','other') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT 'other' COMMENT '应用平台类型',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '租户-应用' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '租户-项目' ROW_FORMAT = DYNAMIC;
---
+++ update/store_app_url.sql
@@ -0,0 +1,11 @@
+DROP PROCEDURE IF EXISTS store_app_url;
+
+CREATE PROCEDURE `store_app_url`()
+BEGIN
+	IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'yc_store_app' AND column_name = 'url')
+	THEN
+		ALTER TABLE `yc_store_app` ADD COLUMN `url` varchar(255) NULL COMMENT '项目网址，不带结尾斜杠' AFTER `name`;
+	END IF;
+END
+
+CALL store_app_url();
