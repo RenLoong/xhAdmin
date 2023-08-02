c9b60df3ed987135ae9db4c5c61a0b3a869be6c2
楚羽幽 <cy958416459@qq.com>
2023-08-02 17:31:09 +0800

    1

--- update/store_app_url.sql
+++ update/store_app_url.sql
@@ -1,11 +1,14 @@
 DROP PROCEDURE IF EXISTS store_app_url;
 
+DELIMITER //
+
 CREATE PROCEDURE store_app_url()
 BEGIN
-	IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'yc_store_app' AND column_name = 'url')
-	THEN
-		ALTER TABLE `yc_store_app` ADD COLUMN `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '项目网址，不带结尾斜杠' AFTER `name`;
-	END IF;
-END;
+    IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'yc_store_app' AND column_name = 'url') THEN
+        ALTER TABLE `php_store_app` ADD COLUMN `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '项目网址，不带结尾斜杠' AFTER `name`;
+    END IF;
+END //
+
+DELIMITER ;
 
 CALL store_app_url();
