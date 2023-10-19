037c8f1c5534a0cd64fa46ce0ac14f7dea4b8c33
楚羽幽 <cy958416459@qq.com>
2023-10-19 18:41:38 +0800

    移除应用插件用户表，应用文章表增加字段

--- public/install/http/data/sql/yc_plugin_articles.sql
+++ public/install/http/data/sql/yc_plugin_articles.sql
@@ -7,8 +7,11 @@ CREATE TABLE `__PREFIX__plugin_articles` (
   `saas_appid` int(11) DEFAULT NULL COMMENT '项目ID',
   `cid` int(11) DEFAULT NULL COMMENT '分类ID',
   `title` varchar(255) NOT NULL DEFAULT '' COMMENT '标题',
+  `desc` varchar(255) DEFAULT NULL COMMENT '简短描述',
+  `thumb` varchar(255) DEFAULT NULL COMMENT '文章封面',
   `status` enum('10','20') DEFAULT '10' COMMENT '状态：10禁用，20启用',
   `content` text COMMENT '内容',
   `view` int(11) DEFAULT '0' COMMENT '热度',
+  `virtually_view` int(11) DEFAULT '0' COMMENT '虚拟热度',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='应用插件-文章内容';
