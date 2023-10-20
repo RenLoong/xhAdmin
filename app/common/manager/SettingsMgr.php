e4726614d49a3440c89e0c8f93db625a0b26d458
楚羽幽 <cy958416459@qq.com>
2023-10-20 14:06:04 +0800

    优化settings管理器

--- app/common/manager/SettingsMgr.php
+++ app/common/manager/SettingsMgr.php
@@ -2,7 +2,6 @@
 namespace app\common\manager;
 
 use app\common\model\SystemConfig;
-use support\Request;
 
 /**
  * 系统设置管理
@@ -12,55 +11,48 @@ use support\Request;
 class SettingsMgr
 {
     /**
-     * 请求对象
-     * @var Request
-     */
-    protected $request = null;
-
-    /**
-     * 应用ID（null则获取系统配置）
-     * @var string|null
-     */
-    protected $appid = null;
-
-    /**
-     * 构造函数
-     * @param mixed $appid
-     * @author 贵州猿创科技有限公司
-     * @copyright 贵州猿创科技有限公司
-     * @email 416716328@qq.com
-     */
-    public function __construct($appid = null)
-    {
-        $this->request = request();
-        $this->appid = $appid;
-    }
-
-    /**
-     * 获取单项配置
-     * @param mixed $name
-     * @param mixed $appid
-     * @return string
-     * @author John
-     */
-    public function get(string $name)
-    {
-        return '';
-    }
-
-    /**
-     * 获取配置分组数据
-     * @param string $group
+     * 获取配置项数据
+     * @param int|null $appid
+     * @param string $name
+     * @param string $default
      * @return mixed
      * @author 贵州猿创科技有限公司
      * @copyright 贵州猿创科技有限公司
      * @email 416716328@qq.com
      */
-    public function group(string $group)
+    public static function get(int|null $appid,string $name,string $default=null)
     {
         $where = [
-            'group'     => $group
+            'name'      => $name,
+            'appid'     => $appid
         ];
-        return SystemConfig::where($where)->value('value');
+        $value = SystemConfig::where($where)->value('value');
+        if(empty($value)){
+            return $default;
+        }
+        return $value;
+    }
+
+    /**
+     * 获取配置分组数据
+     * @param int|null $appid
+     * @param string $group
+     * @param string $default
+     * @return mixed
+     * @author 贵州猿创科技有限公司
+     * @copyright 贵州猿创科技有限公司
+     * @email 416716328@qq.com
+     */
+    public static function group(int|null $appid,string $group,string $default=null)
+    {
+        $where = [
+            'group'     => $group,
+            'appid'     => $appid
+        ];
+        $value = SystemConfig::where($where)->column('value');
+        if(empty($value)){
+            return $default;
+        }
+        return $value;
     }
 }
