ea82710304ec010d3e25201841ed38dbc6734332
楚羽幽 <cy958416459@qq.com>
2023-10-25 15:54:37 +0000

    update update/store.sql.
    Signed-off-by: 楚羽幽 <cy958416459@qq.com>

--- update/store.sql
+++ update/store.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `yc_store` 
-ADD COLUMN `is_uploadify` enum('10','20') NULL DEFAULT '10' COMMENT '附件库权限：10 无权限，20有权限' AFTER `is_uploadify`;
+ADD COLUMN `is_uploadify` enum('10','20') NULL DEFAULT '10' COMMENT '附件库权限：10 无权限，20有权限' AFTER `other`;
