7ccf2a81001d25f51a75f3734cd3e890d88fbd5f
楚羽幽 <cy958416459@qq.com>
2023-11-16 17:48:23 +0800

    1

--- public/install/http/data/sql/yc_plugin_tags.sql
+++ public/install/http/data/sql/yc_plugin_tags.sql
@@ -6,6 +6,7 @@ CREATE TABLE `__PREFIX__plugin_tags` (
   `update_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   `saas_appid` int(11) DEFAULT NULL COMMENT '项目ID',
   `name` varchar(50) DEFAULT NULL COMMENT '标签名称',
+  `menu_title` varchar(30) DEFAULT NULL COMMENT '菜单标题',
   `sort` int(11) NULL DEFAULT 100 COMMENT '排序',
   `title` varchar(255) NOT NULL DEFAULT '' COMMENT '标题',
   `status` enum('10','20') DEFAULT '10' COMMENT '状态：10禁用，20启用',
